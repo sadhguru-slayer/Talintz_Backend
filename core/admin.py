7131584dc3550fc0aa5575ce35ed3eb5b672c00b
Sadgruru <sadgurustreak@gmail.com>
2025-06-30 17:46:35 +0530

    Changed adding user mechanism and solved the issue of Not loggin in with the password

--- core/admin.py
+++ core/admin.py
@@ -1,13 +1,40 @@
 from django.contrib import admin
 from .models import *
 from django_celery_beat.models import PeriodicTask, IntervalSchedule
+from django.contrib.auth.hashers import make_password
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'role', 'is_email_verified', 'is_active')
+    list_filter = ('role', 'is_active', 'is_email_verified')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone_number', 'nickname')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Custom fields', {'fields': ('role', 'membership', 'is_profiled', 'is_talentrise', 'is_email_verified', 'is_phone_verified', 'referral_code')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'role', 'membership'),
+        }),
+    )
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
+
+    def save_model(self, request, obj, form, change):
+        # Hash the password if it's being set or changed
+        if 'password' in form.changed_data:
+            obj.password = make_password(obj.password)
+        super().save_model(request, obj, form, change)
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Connection)
 admin.site.register(Category)
 admin.site.register(Skill)
+
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ['title','id', 'client', 'domain', 'status']
+    list_display = ['title', 'id', 'client', 'domain', 'status']
     filter_horizontal = ('skills_required',)
 
 admin.site.register(Project, ProjectAdmin)
@@ -16,7 +43,6 @@ admin.site.register(Task)
 admin.site.register(Payment)
 admin.site.register(UserFeedback)
 admin.site.register(Notification)
-
 
 class BidAdmin(admin.ModelAdmin):
     list_display = ['id', 'freelancer', 'project', 'bid_type', 'state', 'total_value', 'created_at']
